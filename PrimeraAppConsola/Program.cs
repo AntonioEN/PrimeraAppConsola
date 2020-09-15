94d93599d6d470b537b7c7cc573b2965df750568
Antonio Espinosa <espianton@gmail.com>
2020-09-15 10:14:05 +0200

    Actualizacion al castellano

--- PrimeraAppConsola/Program.cs
+++ PrimeraAppConsola/Program.cs
@@ -6,7 +6,8 @@ namespace PrimeraAppConsola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Actualizacion del mensaje en castellano
+            Console.WriteLine("Hola Mundo!");
         }
     }
 }
